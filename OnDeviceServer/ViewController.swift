f32eed23d530e6aedb5cb794a17f0f79d21f3a2e
Shayoke Mukherjee <shayoke@gmail.com>
2019-06-17 21:24:17 +0100

    Initial Commit

---
+++ OnDeviceServer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnDeviceServer
+//
+//  Created by Shayoke Mukherjee on 17/06/2019.
+//  Copyright © 2019 Shayoke Mukherjee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
